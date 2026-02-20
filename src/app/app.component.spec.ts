9d123ff503ad80afe8126e0b15c520f623ba2f61
arturo9leon <151701214+arturo9leon@users.noreply.github.com>
2026-02-19 21:31:31 -0500

    nuevo proyecto

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'proyect-dash' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('proyect-dash');
-  });
-
-  it('should render title', () => {
+  it('should render login title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, proyect-dash');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Iniciar sesión');
   });
 });
